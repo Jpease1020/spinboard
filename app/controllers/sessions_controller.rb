b8885639feddc69eff17896a0302c2e2b697ed98
Justin Pease <justinpease2@gmail.com>
2016-04-26 10:47:15 -0600

    adds sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(:email_address, params[:user][:email_address])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      flash.now[:error] = "Invalid Login. Try Again."
+      render :new
+    end
+
   end
 end
