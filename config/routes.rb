2de9a74e5f9dbfea8a927cde7952d288e4dd34bc
Justin Pease <justinpease2@gmail.com>
2016-04-26 10:41:17 -0600

    adds routes for loging in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   get '/dashboard', to: 'users#show'
   resources :users, only: [:new, :create]
 end
