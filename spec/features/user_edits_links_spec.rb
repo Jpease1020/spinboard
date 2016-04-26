d184ffd46058724f0f83c27febd4c9a087acc59a
Justin Pease <justinpease2@gmail.com>
2016-04-26 14:36:26 -0600

    adds sad path for user changing url with invalid url

--- spec/features/user_edits_links_spec.rb
+++ spec/features/user_edits_links_spec.rb
@@ -17,4 +17,38 @@ RSpec.describe "User edits links", type: :feature do
 
     expect(page).to have_content("A new title")
   end
+
+  scenario "user changes link url", js: true do
+
+    user = User.create!(email_address: 'justin@email.com', password: 'pass')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    link = Link.create!(title: "a title", url: "http://url.com", user_id: user.id)
+    visit links_path
+
+    el = find(:xpath, "//span[@contenteditable='true' and @class='url']")
+    el.set("http://anewurl.com")
+    el.native.send_keys(:return)
+
+    visit links_path
+
+    expect(page).to have_content("http://anewurl.com")
+  end
+
+  scenario "tries to edit the link url with an invlaid url", js: true do
+
+    user = User.create!(email_address: 'justin@email.com', password: 'pass')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    link = Link.create!(title: "a title", url: "http://url.com", user_id: user.id)
+    visit links_path
+
+    el = find(:xpath, "//span[@contenteditable='true' and @class='url']")
+    el.set("newurl.com")
+    el.native.send_keys(:return)
+
+    visit links_path
+
+    expect(page).to have_content("http://url.com")
+  end
 end
