b4ef0a66ce067fabfcd9501a274c10b2c7ac5247
Justin Pease <justinpease2@gmail.com>
2016-04-26 09:46:26 -0600

    adds create method and logic for creating user and user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = 'user successfully created'
+      redirect_to dashboard_path
+    else
+      flash[:alert] = user.errors.full_messages.first
+      redirect_to action: 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password, :password_confirmation)
+  end
 end
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -11,16 +11,15 @@ RSpec.feature "guest visits home page" do
     fill_in "Confirm password", with: "password"
     click_on "Create"
     expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Hi user")
   end
 
 	scenario "account creation fails without unique email" do
-    user = User.create(email: "a@email.com", password_digest: "password")
+    user = User.create(email_address: "a@email.com", password_digest: "password")
 		visit root_path
     click_on "Sign Up"
     expect(current_path).to eq("/users/new")
 
-    fill_in "Email", with: user.email
+    fill_in "Email", with: user.email_address
     fill_in "Password", with: "password"
     fill_in "Confirm password", with: "password"
     click_on "Create"
