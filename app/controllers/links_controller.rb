6ba5aa10d5dc16171e74cf6070958df234edd1c6
Justin Pease <justinpease2@gmail.com>
2016-04-26 11:17:30 -0600

    adds create action and model for links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,5 +4,20 @@ class LinksController < ApplicationController
   end
 
   def create
+    link = Link.new(link_params)
+    link.user_id = current_user.id
+    if link.save
+      flash[:notice] = "Link Created"
+      redirect_to links_path
+    else
+      flash[:error] = "Invalid Link"
+      redirect_to links_path
+    end
   end
+  private
+
+  def links_params
+    params.require(:link).permit(:title, :url)
+  end
+
 end
